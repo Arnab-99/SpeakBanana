dae4772b0b869b56ef7ed694ee727f3f8dcdc7aa
Arnab Ghosh <ghosh.arnab365@gmail.com>
2021-08-14 01:15:47 +0530

    errorHandler

--- MINION/app.js
+++ MINION/app.js
@@ -8,6 +8,11 @@ function getURL(input){
     return serverURL + "?text=" + input;
 }
 
+function errorHandler(error){
+    console.log("error occured "+error);
+    alert("Something went wrong with the server! Please Try Again Later")
+}
+
 function clickHandler(){
     var inputText = txtInput.value;
     fetch(getURL(inputText))
@@ -15,7 +20,8 @@ function clickHandler(){
     .then(json => {
         var translatedText = json.contents.translated;
         outputDiv.innerText = translatedText;
-    }) 
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click",clickHandler);
